8a9a3f8bf7996528628a527728f976f5dba20350
John Ross <johnross@Johns-MacBook-Pro.local>
2018-08-28 21:09:27 -0500

    fixed problem with activeItem state setting when adding a new item

--- src/components/ItemModal.js
+++ src/components/ItemModal.js
@@ -44,7 +44,7 @@ class ItemModal extends Component {
 
     componentWillMount() {
         ReactModal.setAppElement('body');
-        if (Object.keys(this.props.activeItem).length > 0) {
+        if (this.props.activeItem.name) {
             this.setState({...this.props.activeItem});
         }
     }
@@ -61,12 +61,24 @@ class ItemModal extends Component {
         fetch(`${REACT_APP_LOOKUP_URL}/${REACT_APP_LOOKUP_KEY}/${upc}`)
         .then(res => res.json())
         .catch(error => error)
-        .then(res => this.setState({
-            upc: upc,
-            name: res.items[0].name,
-            price: `$${res.items[0].salePrice.toFixed(2)}`,
-            img_url: res.items[0].thumbnailImage
-        }))
+        .then(res => {
+            if (res.errors) {
+                this.setState({
+                    upc: upc,
+                    name: 'Item not found at Walmart',
+                    price: `No price data`,
+                    img_url: ''
+                })
+            }
+            else {
+                this.setState({
+                    upc: upc,
+                    name: res.items[0].name,
+                    price: `$${res.items[0].salePrice.toFixed(2)}`,
+                    img_url: res.items[0].thumbnailImage
+                })
+            }
+        })
     }
     
     _onDetected(results) {
